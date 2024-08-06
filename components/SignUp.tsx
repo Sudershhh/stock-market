58658b139e76d972f7dabcca96dd0ee6950971c8
Sri Sudersan <tgsuder@gmail.com>
2024-08-06 10:38:18 -0400

    Refactored Sign Up component - added css

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { Button, Input } from "@nextui-org/react";
 import { MailIcon, VenetianMask } from "lucide-react";
-
+import Image from "next/image";
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -54,73 +54,92 @@ const SignUp = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      {/* <ToastContainer /> */}
-      <motion.form
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md"
-        onSubmit={handleSignUp}
-      >
-        <h1 className="text-4xl font-bold mb-6 text-green-500">Sign Up</h1>
-        <div className="my-10">
-          <Input
-            value={email}
-            type="email"
-            label="Email"
-            variant="bordered"
-            isInvalid={isInvalid}
-            color={isInvalid ? "danger" : "success"}
-            errorMessage="Please enter a valid email"
-            onValueChange={setEmail}
-            labelPlacement="outside"
-            className=" w-full"
-            startContent={<MailIcon />}
-          />
-        </div>
-        <div className="mb-14">
-          <Input
-            value={password}
-            type="password"
-            label="Password"
-            variant="bordered"
-            onValueChange={setPassword}
-            className="w-full "
-            color="success"
-            labelPlacement="outside"
-            startContent={<VenetianMask color="white" />}
-          />
-        </div>
-        <div className="my-6">
-          <Input
-            value={confirmPassword}
-            type="password"
-            label="Confirm Password"
-            variant="bordered"
-            onValueChange={setConfirmPassword}
-            className="w-full "
-            color="success"
-            labelPlacement="outside"
-            startContent={<VenetianMask color="white" />}
-          />
-        </div>
-
-        <div className="flex items-center justify-between mb-4">
-          <button
-            type="submit"
-            className="bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline transition duration-300"
-          >
+    <div className="min-h-screen">
+      <div className="p-8">
+        <Image
+          src={"/logo.png"}
+          alt="Stock Market Logo"
+          width={300}
+          height={80}
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
+        />
+      </div>
+      <div className="flex items-center justify-around mt-24">
+        {/* <ToastContainer /> */}
+        <motion.form
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className=" p-8 rounded-lg w-full max-w-md"
+          onSubmit={handleSignUp}
+        >
+          <h1 className="text-3xl font-extrabold mb-16 text-center text-secondary-300">
             Sign Up
-          </button>
-          <Link
-            href="/sign-in"
-            className="inline-block align-baseline font-medium text-sm text-green-500 hover:text-green-700 transition duration-300"
-          >
-            Already have an account? Sign In
-          </Link>
-        </div>
-      </motion.form>
+          </h1>
+          <div className="my-10">
+            <Input
+              value={email}
+              type="email"
+              label="Email"
+              variant="bordered"
+              isInvalid={isInvalid}
+              color={isInvalid ? "danger" : "secondary"}
+              errorMessage="Please enter a valid email"
+              onValueChange={setEmail}
+              labelPlacement="outside"
+              className="w-full text-secondary-400"
+              placeholder="example@email.com"
+              startContent={<MailIcon color="#cbacf9" />}
+            />
+          </div>
+          <div className="mb-14">
+            <Input
+              value={password}
+              type="password"
+              label="Password"
+              variant="bordered"
+              onValueChange={setPassword}
+              className="w-full text-secondary-400"
+              color="secondary"
+              labelPlacement="outside"
+              startContent={<VenetianMask color="#cbacf9" />}
+            />
+          </div>
+          <div className="my-6">
+            <Input
+              value={confirmPassword}
+              type="password"
+              label="Confirm Password"
+              variant="bordered"
+              onValueChange={setConfirmPassword}
+              className="w-full text-secondary-400"
+              color="secondary"
+              labelPlacement="outside"
+              startContent={<VenetianMask color="#cbacf9" />}
+            />
+          </div>
+          <div className="flex items-center justify-between mb-4">
+            {/* <button
+              type="submit"
+              className="bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-6 rounded focus:outline-none focus:shadow-outline transition duration-300"
+            >
+              Sign Up
+            </button> */}
+
+            <Button color="secondary" type="submit">
+              Sign Up
+            </Button>
+
+            <Link
+              href="/sign-in"
+              className="inline-block align-baseline font-medium text-sm text-secondary-300 hover:text-secondary-400 transition duration-300"
+            >
+              Already have an account? Sign In
+            </Link>
+          </div>
+        </motion.form>
+      </div>
     </div>
   );
 };
