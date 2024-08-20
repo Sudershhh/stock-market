2d6f5a0305c7cce1b00efae234e447086d30651b
Sri Sudersan <tgsuder@gmail.com>
2024-08-19 23:41:49 -0400

    Refactored Components into app folder

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,70 @@
-import News from "@/components/News/News";
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { newsApi } from "@/public/dummyData/newsApi";
+import NewsContainer from "@/components/News/NewsContainer";
+import { NewsArticle } from "@/types/newsApi";
+import Header from "@/components/Header";
+async function News() {
+  const [news, setNews] = useState<NewsArticle[]>(newsApi);
+  const [newsFetchedError, setNewsFetchedError] = useState();
 
-function page() {
-  return <News />;
+  //   const financeArticleEndpoint = process.env
+  //     .NEXT_PUBLIC_FETCH_FINANCE_ARTICLES_ENDPOINT as string;
+  //   const newsApiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY as string;
+  // useEffect(() => {
+  //   async function fetchNewsArticles() {
+  //     setNews(newsApi);
+  //     //   try {
+  //     //     const response = await axios.post(financeArticleEndpoint, {
+  //     //       query: {
+  //     //         $query: {
+  //     //           $and: [
+  //     //             { conceptUri: "http://en.wikipedia.org/wiki/Stock_market" },
+  //     //             { conceptUri: "http://en.wikipedia.org/wiki/Finance" },
+  //     //             { conceptUri: "http://en.wikipedia.org/wiki/United_States" },
+  //     //             {
+  //     //               dateStart: "2024-07-20",
+  //     //               dateEnd: "2024-07-27",
+  //     //               lang: "eng",
+  //     //             },
+  //     //           ],
+  //     //         },
+  //     //         $filter: {
+  //     //           startSourceRankPercentile: 0,
+  //     //           endSourceRankPercentile: 30,
+  //     //         },
+  //     //       },
+  //     //       resultType: "articles",
+  //     //       articlesSortBy: "date",
+  //     //       includeArticleImage: true,
+  //     //       apiKey: newsApiKey,
+  //     //     });
+  //     //     console.log(response.data);
+  //     //     setNews(response.data.articles.results);
+  //     //   } catch (error: any) {
+  //     //     console.error("Error fetching news:", error);
+  //     //     setNewsFetchedError(error);
+  //     //   }
+  //   }
+
+  //   fetchNewsArticles();
+  // }, []);
+
+  return (
+    <div className="p-8 min-h-screen">
+      <Header />
+      <div className="m-24">
+        <h1 className="text-7xl mt-24 mb-8">The Stock Market Times</h1>
+
+        <div className="news-articles flex flex-wrap justify-between align-middle">
+          {news.map((newsArticle: NewsArticle, index: number) => (
+            <NewsContainer key={index} newsArticle={newsArticle} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page;
+export default News;
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,101 @@
-import SignIn from "../../components/SignIn";
+"use client";
 
-export default function SignInPage() {
-  return <SignIn />;
-}
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { useAuthStore } from "@/lib/authStore";
+import { Button } from "@nextui-org/react";
+import Image from "next/image";
+import PasswordInput from "@/components/ui/PasswordInput";
+import EmailInput from "@/components/ui/EmailInput";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const { signIn, loading, error } = useAuthStore();
+
+  const handleSignIn = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const signedInPromise = await signIn(email, password);
+
+      router.push("/");
+      localStorage.setItem("isSignedIn", "true");
+    } catch (errorMessage) {
+      toast.error(error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white overflow-hidden">
+      <div className="p-8">
+        <Image
+          src={"/logo.png"}
+          alt="Stock Market Logo"
+          width={300}
+          height={80}
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
+        />
+      </div>
+      <div className="flex flex-col md:flex-row items-center justify-around mt-8 md:mt-24 w-full max-w-screen-lg">
+        <motion.form
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className=" p-8 rounded-lg w-full max-w-md"
+          onSubmit={handleSignIn}
+        >
+          <h1 className="text-3xl font-extrabold mb-16 text-center text-secondary-300">
+            Sign In
+          </h1>
+          <div className="mt-8">
+            <EmailInput email={email} onEmailChange={setEmail} />
+          </div>
+
+          <PasswordInput
+            label="Password"
+            value={password}
+            onValueChange={setPassword}
+          />
+          <div className="flex items-center justify-between mb-4">
+            <Button color="secondary" type="submit" isDisabled={loading}>
+              Sign In
+            </Button>
+
+            <Link
+              href="/sign-up"
+              className="inline-block align-baseline font-medium text-sm text-secondary-300 hover:text-secondary-400 transition duration-300"
+            >
+              Don't have an account? Sign Up
+            </Link>
+          </div>
+        </motion.form>
+
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          className="relative mt-12 md:mt-0"
+        >
+          <Image
+            src={"/stocketa-track.webp"}
+            alt="Stock Market Logo"
+            width={240}
+            height={100}
+            className="rounded-xl relative"
+            style={{ objectFit: "cover" }}
+          />
+          <div className="absolute top-0 right-0 w-full h-full bg-gradient-to-t from-black via-transparent to-transparent rounded-xl"></div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
